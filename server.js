186e891ccca5647230add6e9dafef52394a598d8
David0770 <davidgomesgc230@gmail.com>
2023-11-29 21:21:27 -0300

    Adiciona rota GET '/data' para acessar os dados de produtos

--- server.js
+++ server.js
@@ -3,7 +3,23 @@ const app = express();
 const fs = require('fs')
 
 
+
 app.get('/', (req, res) => {
+  res.send('<h1>Pagina Home</h1>')
+
+})
+
+
+app.get('/data', (req, res) => {
+  fs.readFile('db.json', 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err)
+      res.status(500).send('Erro ao Processar a solicitação')
+    }
+
+    const jsonData = JSON.parse(data);
+    res.json(jsonData)
+  })
 
 })
 
